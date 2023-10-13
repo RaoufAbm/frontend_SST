ae1259c0f0cef8d33749cf90def2e1191c0c1049
RaoufAbm <raoufabm5@gmail.com>
2023-10-13 19:31:26 +0100

    table

--- app/pages/musurs/[cuve]/page.tsx
+++ app/pages/musurs/[cuve]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import styles from "/app/page.module.css";
-import TableMusurs from '@/components/tableMusurs';
+import TableMusurs from '@/components/table_musurs';
 
  function tableCuve({params}) {
   const id=(params.cuve);
