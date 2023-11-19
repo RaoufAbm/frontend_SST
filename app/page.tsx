da581f59957aa0d1f36e2e7324fc93e6a570e2d0
RaoufAbm <raoufabm5@gmail.com>
2023-11-20 00:36:32 +0100

    dernier modification tableau

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+// "use client";
 import Login from "@/components/login";
-
+// import { useEffect, useState } from "react";
+// import ClipLoader from "react-spinners/ClipLoader";
 
 function login() {
+// const [loading,setLoading]=useState(false)
+// useEffect(()=>{
+//   setLoading(true)
+//   setTimeout(() => {
+//     setLoading(false)
+//   }, 1500);
+// })
   return (
-       
+       <>
+       {/* {
+        loading?
+        <ClipLoader
+        color={"black"}
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+      : */}
         <Login/>
+       {/* } */}
+       </>
 
   );
 }
